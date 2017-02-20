79960a867964c4e5489fcec974372f6f42eea225
Rafael Santos <rafaels@gmail.com>
2017-02-20 05:43:25 -0500

    loaded and saved images basic example

---
+++ load_display_save.py
@@ -0,0 +1,23 @@
+import argparse, cv2, os
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="Path to the image")
+ap.add_argument("-ni", "--new-image", required=False, help="Path to the image to be saved")
+args = vars(ap.parse_args())
+print(args)
+
+image = cv2.imread(args["image"])
+print("width: {} pixels".format(image.shape[1]))
+print("height: {} pixels".format(image.shape[0]))
+print("channels: {}".format(image.shape[2]))
+
+cv2.imshow("Image", image)
+cv2.waitKey(0)
+
+if args["new_image"]:
+	try:
+		cv2.imwrite(args["new_image"], image)
+	except IOError as e:
+		print("couldn't save new image {}: {}".format(args["new_image"], e.strerror))
+	except:
+		print("couldn't save new image")
