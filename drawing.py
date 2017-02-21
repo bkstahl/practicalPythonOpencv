493390710d21ecb6c7c86f73797b2e2d3f7f35e2
Rafael Santos <rafaels@gmail.com>
2017-02-20 20:23:24 -0500

    finished chapter 5

--- drawing.py
+++ drawing.py
@@ -29,3 +29,12 @@ for r in range(0, 175, 25):
 
 cv2.imshow("Canvas", canvas)
 cv2.waitKey(0)
+
+for i in range(25):
+    radius = np.random.randint(5, high = 200)
+    color = np.random.randint(0, high = 255, size = (3,)).tolist()
+    pt = np.random.randint(0, high = 300, size = (2,))
+    cv2.circle(canvas, tuple(pt), radius, color, -1)
+
+cv2.imshow("Canvas", canvas)
+cv2.waitKey(0)
