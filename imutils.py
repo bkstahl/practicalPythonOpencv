38d6eda94ae483353b46d4499fa42ffe0a9eb001
Rafael Santos <rafaels@gmail.com>
2017-02-21 11:11:45 -0500

    finished the rotation part of chapter 6

--- imutils.py
+++ imutils.py
@@ -5,3 +5,11 @@ def translate(image, x, y):
     M = np.float32([[1, 0, x], [0, 1, y]])
     shifted = cv2.warpAffine(image, M, (image.shape[1], image.shape[0]))
     return shifted
+
+def rotate(image, angle, center = None, scale = 1):
+    if center is None:
+        (w, h) = (image.shape[1], image.shape[0])
+        center = (w//2, h//2)
+    M = cv2.getRotationMatrix2D(center, angle, scale)
+    rotated = cv2.warpAffine(image, M, (w, h))
+    return rotated
--- translation.py
+++ translation.py
@@ -7,6 +7,8 @@ args = vars(ap.parse_args())
 image = cv2.imread(args["image"])
 cv2.imshow("Original", image)
 
+# translation
+# translation via matrices
 M = np.float32([[1, 0, 25], [0, 1, 50]])
 shifted = cv2.warpAffine(image, M, (image.shape[1], image.shape[0]))
 cv2.imshow("Shifted Down and Right", shifted)
@@ -15,6 +17,7 @@ M = np.float32([[1, 0, -50], [0, 1, -90]])
 shifted = cv2.warpAffine(image, M, (image.shape[1], image.shape[0]))
 cv2.imshow("Shifted Up and Left", shifted)
 
+# translation via helper function
 shifted = imutils.translate(image, 10, -100)
 cv2.imshow("Shifted with utility function", shifted)
 
